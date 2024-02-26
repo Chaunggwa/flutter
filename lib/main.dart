86ee6a7e19c0e8a363ba25b3d2689bf1d3e46e67
Thu Rein Soe <nautical19thureinsoe@gmail.com>
2024-02-26 11:27:10 +0630

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:testing1/views/login_view.dart';
+import 'package:testing1/views/register_view.dart';
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user!.emailVerified) {
+                print('You are a verified user.');
+              } else {
+                print('You need to verify your email first.');
+              }
+              return Text('Done.');
+            // TODO: Handle this case.
+            default:
+              return const Text("Loading...");
+          }
+        },
+      ),
+    );
+  }
+}
